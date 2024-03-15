685ff3c5947c2da40d46fe7855cbcdb2c4fb901a
diego <diego@DGMOLAR-ACER>
2024-03-14 21:31:58 -0600

    Correccion detalles h ventas

--- SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
+++ SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa1b36f311c42c670ea23ee696fa88c093f630a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4fc2ef217010ad63b0e54f96ba65a713a323e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
